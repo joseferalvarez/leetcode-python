7bb4003d2b16760be543d50a0dff6c51189e59ce
Jose Fernando Álvarez Romero <joseferalvarezromero@gmail.com>
2024-08-04 19:02:43 +0200

    second problem solved

---
+++ palindrome.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+       return True if str(x)[::-1] == str(x) else False
+        
+solution = Solution()
+print(solution.isPalindrome(121))
+print(solution.isPalindrome(-121))
